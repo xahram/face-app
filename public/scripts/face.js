29ee62111e31dd67885eb96aadd71048cc74be6d
xahram <ssasuke1163@gmail.com>
2020-08-26 20:54:41 +0500

    face detection done onchange det done canvas writing done

--- public/scripts/face.js
+++ public/scripts/face.js
@@ -4,26 +4,11 @@ var img = document.getElementById('img')
 var inputField = document.getElementById('imgUpload')
 var canvas = document.getElementById('canvas')
 
-Promise.all([
-    faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
-    // net.loadFromUri(MODEL_URL),
-    faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-    faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-]).then((val) => {
-    faceapi.detectSingleFace(img).then((value) => {
-    console.log(value)
-}).catch((err) => { console.log(err) })
-
-}).catch((err) => {
-    console.log(err)
-})
-
-
-var inputChangeListner = function (e) {
+var inputChangeListner = async function (e) {
     var selectedFile = e.target.files[0];
     var reader = new FileReader();
 
-   // // var imgtag = document.getElementById("img");
+    // // var imgtag = document.getElementById("img");
     img.title = selectedFile.name;
 
     reader.onload = function (event) {
@@ -32,8 +17,37 @@ var inputChangeListner = function (e) {
     };
 
     reader.readAsDataURL(selectedFile);
+    const detections = await faceapi.detectSingleFace(img)
+    const imageDimensions = { width: img.width, height: img.height }
+    const resizedDetections = faceapi.resizeResults(detections, imageDimensions)
+    faceapi.draw.drawDetections(canvas, detections)
 }
 inputField.onchange = inputChangeListner
+
+
+
+
+const modelLoadingFaceApi = async () => {
+    const modelTraining = await Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+        // net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+    ])
+    return modelTraining
+}
+modelLoadingFaceApi().then(async (val) => {
+    const detections = await faceapi.detectSingleFace(img)
+    console.log(detections)
+    const imageDimensions = { width: img.width, height: img.height }
+
+    // const canvasResizing = { width: detections._box.width, height: detections._box.height }
+    const canvasResizing = { width: img.width, height: img.height }
+    faceapi.matchDimensions(canvas, canvasResizing)
+
+    const resizedDetections = faceapi.resizeResults(detections, imageDimensions)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+}).catch((err) => { console.log(err) })
 
 
 
